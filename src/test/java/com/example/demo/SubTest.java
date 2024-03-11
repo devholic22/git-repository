67505b67fef265d18d7154ce9de3112fa15977e9
devholic22 <hyunjoon.tech@gmail.com>
2024-03-12 01:27:43 +0900

    Create SubTest.java

---
+++ src/test/java/com/example/demo/SubTest.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class SubTest {
+
+    @Test
+    void test() {
+        int a = 3;
+        int b = 5;
+        assertThat(a + b).isEqualTo(9); // 테스트 실패 예
+    }
+}
